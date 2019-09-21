d51c97cf11da0d4f558e80feb6e230dfe05c5671
Luan da Costa Redmann <luandacostaredmann@gmail.com>
2019-09-21 20:36:44 -0300

    Adiciona uma estrutura de tupla dentro do model SalaDeAula.

--- src/projeto/otime/models.py
+++ src/projeto/otime/models.py
@@ -3,3 +3,10 @@ from django.db import models
 # Create your models here.
 class SalaDeAula(models.Model):
     nome = models.CharField(max_length = 40)
+    tipos = (
+        ("Sala regular", "Sala regular"),
+        ("Laboratório", "Laboratório"),
+        ("Audiovisual", "Audiovisual"),
+        ("Auditório", "Auditório"),
+        ("Miniauditório", "Miniauditório"),
+    )
