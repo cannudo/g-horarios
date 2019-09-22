b18f3592537769f46685e57e10415837a491ce49
Luan da Costa Redmann <luandacostaredmann@gmail.com>
2019-09-22 19:08:25 -0300

    Pega uma lista de objetos no banco e a coloca na variável de contexto

--- src/projeto/otime/views.py
+++ src/projeto/otime/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import SalaDeAula
 
 # Create your views here.
 def index(request):
+    lista_de_salas = SalaDeAula.objects.all()
+    contexto = {
+        "lista_de_salas": lista_de_salas
+    }
     return HttpResponse("Olá, mundo! Isto é temporário.")
