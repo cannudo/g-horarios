3f19c5f224b7b379b9ac9bd5bd9eacc6e2cbd6e8
Luan da Costa Redmann <luandacostaredmann@gmail.com>
2019-09-22 19:29:28 -0300

    Corrige o código para pegar a lista de disciplina

--- src/projeto/otime/views.py
+++ src/projeto/otime/views.py
@@ -4,7 +4,7 @@ from .models import SalaDeAula, Disciplina
 # Create your views here.
 def index(request):
     lista_de_salas = SalaDeAula.objects.all()
-    lista_de_disciplinas = Disciplinas.objects.all()
+    lista_de_disciplinas = Disciplina.objects.all()
     contexto = {
         "lista_de_salas": lista_de_salas,
         "lista_de_disciplinas": lista_de_disciplinas
