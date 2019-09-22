dfeeb47f18c1aa05dbc6be66779535b5b68c9159
Luan da Costa Redmann <luandacostaredmann@gmail.com>
2019-09-22 19:23:23 -0300

    Adiciona uma lista de disciplinas na variável de contexto

--- src/projeto/otime/views.py
+++ src/projeto/otime/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from .models import SalaDeAula
+from .models import SalaDeAula, Disciplina
 
 # Create your views here.
 def index(request):
     lista_de_salas = SalaDeAula.objects.all()
+    lista_de_disciplinas = Disciplinas.objects.all()
     contexto = {
-        "lista_de_salas": lista_de_salas
+        "lista_de_salas": lista_de_salas,
+        "lista_de_disciplinas": lista_de_disciplinas
     }
     return render(request, "otime/index.html", contexto)
