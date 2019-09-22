9f199428cd56ad5c5fce7b152421c7ccabc215b2
Luan da Costa Redmann <luandacostaredmann@gmail.com>
2019-09-22 19:48:37 -0300

    Adiciona uma lista de professores na variável de contexto

--- src/projeto/otime/views.py
+++ src/projeto/otime/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-from .models import SalaDeAula, Disciplina
+from .models import SalaDeAula, Disciplina, Professor
 
 # Create your views here.
 def index(request):
     lista_de_salas = SalaDeAula.objects.all()
     lista_de_disciplinas = Disciplina.objects.all()
+    lista_de_professores = Professor.objects.all()
     contexto = {
         "lista_de_salas": lista_de_salas,
-        "lista_de_disciplinas": lista_de_disciplinas
+        "lista_de_disciplinas": lista_de_disciplinas,
+        "lista_de_professores": lista_de_professores
     }
     return render(request, "otime/index.html", contexto)
