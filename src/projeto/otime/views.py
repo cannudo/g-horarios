827a22a533fecef6e72b4d96e81a7bce38edf160
Luan da Costa Redmann <luandacostaredmann@gmail.com>
2019-09-22 23:48:56 -0300

    Adiciona os dados necessários na variável de contexto para atender aos requisitos do teste em reservar-horario.html

--- src/projeto/otime/views.py
+++ src/projeto/otime/views.py
@@ -14,5 +14,12 @@ def index(request):
     return render(request, "otime/index.html", contexto)
 
 def reservarHorario(request):
-    contexto = {}
+    lista_de_salas = SalaDeAula.objects.all()
+    lista_de_disciplinas = Disciplina.objects.all()
+    lista_de_professores = Professor.objects.all()
+    contexto = {
+        "lista_de_salas": lista_de_salas,
+        "lista_de_disciplinas": lista_de_disciplinas,
+        "lista_de_professores": lista_de_professores
+    }
     return render(request, "otime/reservar-horario.html", contexto)
