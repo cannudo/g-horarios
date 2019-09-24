897b6aa7edc7f93accd5b6c03fb6d77e9190af04
Luan da Costa Redmann <luandacostaredmann@gmail.com>
2019-09-24 08:07:18 -0300

    Exibe o tipo da requisição

--- src/projeto/otime/views.py
+++ src/projeto/otime/views.py
@@ -14,6 +14,8 @@ def index(request):
     return render(request, "otime/index.html", contexto)
 
 def reservarHorario(request):
+    texto = "A requisição foi %s" % request.method
+
     lista_de_salas = SalaDeAula.objects.all()
     lista_de_disciplinas = Disciplina.objects.all()
     lista_de_professores = Professor.objects.all()
@@ -23,5 +25,6 @@ def reservarHorario(request):
         "lista_de_disciplinas": lista_de_disciplinas,
         "lista_de_professores": lista_de_professores,
         "horarios": horarios,
+        "texto": texto,
     }
     return render(request, "otime/reservar-horario.html", contexto)
