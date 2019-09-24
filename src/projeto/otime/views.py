cbcf09aa0231219d08b5a7734c6a385512034341
Luan da Costa Redmann <luandacostaredmann@gmail.com>
2019-09-24 20:33:24 -0300

    Prepara as views para que façam cadastros no banco de dados

--- src/projeto/otime/views.py
+++ src/projeto/otime/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import SalaDeAula, Disciplina, Professor
+from django.shortcuts import render, get_object_or_404
+from .models import *
 
 # Create your views here.
 def index(request):
