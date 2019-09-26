740c13d38a0c6b8ba81a13ab32f152a43f5466d3
Luan da Costa Redmann <luandacostaredmann@gmail.com>
2019-09-25 23:34:54 -0300

    Passa uma lista de slots cadastrados no banco

--- src/projeto/otime/views.py
+++ src/projeto/otime/views.py
@@ -21,12 +21,14 @@ def reservarHorario(request):
         professor = get_object_or_404(Professor, pk = requisicao_post["professor"])
         slot_de_horario = SlotDeHorario(posicao = requisicao_post["posicao"], sala_de_aula = sala_de_aula, disciplina = disciplina, professor = professor)
         slot_de_horario.save()
-        
+
+    slots_de_horario = SlotDeHorario.objects.all()
     lista_de_salas = SalaDeAula.objects.all()
     lista_de_disciplinas = Disciplina.objects.all()
     lista_de_professores = Professor.objects.all()
     horarios = "07h00-07h45 07h45-08h30 09h00-09h45 09h45-10h30 10h30-11h15 11h15-12h00 13h00-13h45 13h45-14h30 14h30-15h15 15h15-16h00 16h30-17h15 15h15-18h00 19h00-19h45 19h45-20h30 20h40-21h25 21h25-22h10"
     contexto = {
+        "slots_de_horario": slots_de_horario,
         "lista_de_salas": lista_de_salas,
         "lista_de_disciplinas": lista_de_disciplinas,
         "lista_de_professores": lista_de_professores,
