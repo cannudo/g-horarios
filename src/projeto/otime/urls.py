4da7257570029eafe430b5771d390b324c12c287
Luan da Costa Redmann <luandacostaredmann@gmail.com>
2019-10-13 18:23:04 -0300

    Remove mais linhas em branco desnecessárias

--- src/projeto/otime/urls.py
+++ src/projeto/otime/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from .views import  index, salas, reservarHorario, professores, disciplinas, lista_professores, atualizar_professor, deletar_professor, criar_professor
 urlpatterns = [
- 	
-   
     path("", index, name = "index"),
     path("salas", salas, name = "salas"),
     path("reservarHorario", reservarHorario, name = "reservarHorario"),
@@ -12,7 +10,4 @@ urlpatterns = [
     path('update/<int:id>/', atualizar_professor, name='atualizar_professor'),
   	path('delete/<int:id>/', deletar_professor, name='deletar_professor'),
    	path('new', criar_professor, name='criar_professores'),
-
-
-   
 ]
