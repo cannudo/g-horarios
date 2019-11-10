959b8380ffdf40c14eb120fb13c3e2b296b53283
Guilherme <eufernandagui154@gmail.com>
2019-11-10 18:30:20 -0300

    Adicionado turma no model de SlotDeHorario.

--- src/projeto/otime/models.py
+++ src/projeto/otime/models.py
@@ -53,6 +53,7 @@ class SlotDeHorario(models.Model):
     sala_de_aula = models.ForeignKey(SalaDeAula, on_delete = models.CASCADE)
     disciplina = models.ForeignKey(Disciplina, on_delete = models.CASCADE)
     professor = models.ForeignKey(Professor, on_delete = models.CASCADE)
+    turma = models.ForeignKey(Turma, on_delete = models.CASCADE)
     def __str__(self):
         return "Slot #%d" % int(self.posicao)
 
