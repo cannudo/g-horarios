23aee4e0a25b6ec4e212635eda1cce43f34ba66b
Guilherme Santos <eufernandagui154@gmail.com>
2019-11-25 00:15:48 -0300

    Adiciona urls para o filtro por professor.

--- src/projeto/otime/urls.py
+++ src/projeto/otime/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path
-from .views import  index, salas, buscas, reservarHorario, exibeSlotTurma, exibeSlotDisciplina, exibeSlotSala, filtroTurma, filtroSala, filtroDisciplina, professores, turmas, disciplinas, seletor_turmas, atualizar_professor, atualizar_disciplina, atualizar_sala, atualizar_turma
+from .views import  index, salas, buscas, reservarHorario, exibeSlotTurma, exibeSlotDisciplina, exibeSlotSala, exibeSlotProfessor, filtroTurma, filtroProfessor, filtroSala, filtroDisciplina, professores, turmas, disciplinas, seletor_turmas, atualizar_professor, atualizar_disciplina, atualizar_sala, atualizar_turma
 urlpatterns = [
     path("", index, name = "index"),
     path("salas", salas, name = "salas"),
     path("buscas", buscas, name = "buscas"),
     path("filtroSala", filtroSala, name = "filtroSala"),
+    path("filtroProfessor", filtroProfessor, name = "filtroProfessor"),
     path("filtroTurma", filtroTurma, name = "filtroTurma"),
     path("filtroDisciplina", filtroDisciplina, name = "filtroDisciplina"),
     path("reservarHorario/<int:id>", reservarHorario, name = "reservarHorario"),
     path("exibeSlotDisciplina/<int:id>", exibeSlotDisciplina, name = "exibeSlotDisciplina"),
     path("exibeSlotTurma/<int:id>", exibeSlotTurma, name = "exibeSlotTurma"),
+    path("exibeSlotProfessor/<int:id>", exibeSlotProfessor, name = "exibeSlotProfessor"),
     path("exibeSlotSala/<int:id>", exibeSlotSala, name = "exibeSlotSala"),
     path("professores", professores, name = "professores"),
     path("seletor_turmas", seletor_turmas, name = "seletor_turmas"),
