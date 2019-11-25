692ed91c25bda5f9c0306081c05e34992eff3728
Guilherme Santos <eufernandagui154@gmail.com>
2019-11-24 22:48:51 -0300

    Adiciona views para filtro por displina.

--- src/projeto/otime/views.py
+++ src/projeto/otime/views.py
@@ -56,7 +56,22 @@ def exibeSlotTurma(request, id):
     }
     return render(request, "otime/exibeSlotTurma.html", contexto)
 
+def exibeSlotDisciplina(request, id):
+    disciplina = get_object_or_404(Disciplina, pk = id)
 
+    slots_de_horario = SlotDeHorario.objects.filter(disciplina = disciplina)
+    lista_de_salas = SalaDeAula.objects.all()
+    lista_de_disciplinas = Disciplina.objects.all()
+    lista_de_professores = Professor.objects.all()
+    horarios = "07h00-07h45 07h45-08h30 09h00-09h45 09h45-10h30 10h30-11h15 11h15-12h00 13h00-13h45 13h45-14h30 14h30-15h15 15h15-16h00 16h30-17h15 15h15-18h00 19h00-19h45 19h45-20h30 20h40-21h25 21h25-22h10"
+    contexto = {
+        "slots_de_horario": slots_de_horario,
+        "lista_de_salas": lista_de_salas,
+        "lista_de_disciplinas": lista_de_disciplinas,
+        "lista_de_professores": lista_de_professores,
+        "horarios": horarios,
+    }
+    return render(request, "otime/exibeSlotDisciplina.html", contexto)
 
 def salas(request):
     form = FormSala(request.POST or None)
@@ -72,6 +87,10 @@ def buscas(request):
 def filtroTurma(request):
     lista_de_turmas = Turma.objects.all()
     return render(request,'otime/filtroTurma.html',{'lista_de_turmas':lista_de_turmas})
+
+def filtroDisciplina(request):
+    lista_de_disciplinas = Disciplina.objects.all()
+    return render(request,'otime/filtroDisciplina.html',{'lista_de_disciplinas':lista_de_disciplinas})
 
 def disciplinas(request):
     form = FormDisciplina(request.POST or None)
